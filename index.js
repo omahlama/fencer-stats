31ab2c8752a07184073051b4306ef9e809a016ed
Olli Mahlamäki <olli@productsense.com>
2026-05-19 09:47:55 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+async function scrape(url) {
+  const { data: html } = await axios.get(url);
+  const $ = cheerio.load(html);
+
+  const cards = $('.card');
+  const data = cards.map((i, card) => {
+    const $card = $(card);
+    return {
+      name: $card.find('h6').text().trim(),
+      url: $card.find('a').attr('href'),
+    };
+  }).get();
+
+  return data;
+}
+
+const url = 'https://fencing.ophardt.online/en/search/biographies?search=aa&search-firstname=&search-nation=FIN&search-gender='
+
+scrape(url).then(console.log);
